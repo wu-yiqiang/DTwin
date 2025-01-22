3ec6e24f4b5aa91fd14e548d0706036e8bbd202d
wu-yiqiang <wu_yiqiang@outlook.com>
2025-01-23 00:29:41 +0800

    项目启动

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="cesiumContainer" ref="cesiumContainer"></div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+<script setup lang="ts">
+import {onMounted} from 'vue'
+import * as Cesium from 'cesium'
+import './Widgets/widgets.css'
+window.CESIUM_BASE_URL = "/"
+onMounted(() => {
+  var view = new Cesium.Viewer('cesiumContainer')
+})
+</script>
+
+<style>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
